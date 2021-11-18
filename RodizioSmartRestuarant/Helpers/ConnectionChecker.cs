927ce76af1d1310b55fc52a12c842d33a424fecd
Yewo Theu <yewotheu123456789@gmail.com>
2021-11-18 11:52:41 +0200

    Connection Checker was fine before

--- RodizioSmartRestuarant/Helpers/ConnectionChecker.cs
+++ RodizioSmartRestuarant/Helpers/ConnectionChecker.cs
@@ -15,7 +15,7 @@ namespace RodizioSmartRestuarant.Helpers
         PingReply result; //I already have code that uses something like this. Its in RodizioSmartRestuarant.Helpers.LANController, check through it to see
         public bool CheckConnection()
         {
-            result = ping.Send(IPAddress.Parse((new OfflineDataContext().GetData("localIP")).ToString()));
+            result = ping.Send(IPAddress.Parse("8.8.8.8"));
 
             if (result.Status == IPStatus.Success)
             {
