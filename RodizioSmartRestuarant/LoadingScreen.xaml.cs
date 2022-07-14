e9c7092085210d40f1086c3f0e32c63a66000e32
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2022-07-14 09:49:47 +0200

    Hotfix Added AutoUpdate Ability Again

--- RodizioSmartRestuarant/LoadingScreen.xaml.cs
+++ RodizioSmartRestuarant/LoadingScreen.xaml.cs
@@ -49,7 +49,7 @@ namespace RodizioSmartRestuarant
         {
             // REFACTOR: I have seen this logic before, consider extracting. But given how I didn't see it too often we might not really need to do so. But consider it,
             //for the obvious benefits
-            /*bool update = false;
+            bool update = false;
             try
             {
                 //Check For Updates
@@ -80,7 +80,6 @@ namespace RodizioSmartRestuarant
             {
                 ;
             }
-            */
             //Some Exists Here Initiailiztion After Mutex Verification
 
             if (FirstTime())
