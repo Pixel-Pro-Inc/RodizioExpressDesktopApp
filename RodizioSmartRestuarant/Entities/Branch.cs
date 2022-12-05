d95419085d2edf52c5b563c90780d2c1be16258e
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2022-12-05 13:18:53 +0200

    Updated Branch Location

--- RodizioSmartRestuarant/Entities/Branch.cs
+++ RodizioSmartRestuarant/Entities/Branch.cs
@@ -8,11 +8,12 @@ namespace RodizioSmartRestuarant.Entities
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string Location { get; set; }
+        public Location Location { get; set; }
         public string ImgUrl { get; set; }
         public string PublicId { get; set; }
         public DateTime LastActive { get; set; }
         public List<int> PhoneNumbers { get; set; }
+        // REFACTOR: Consider having a dictionary here so that we can remove the ClosingTime class
         public List<DateTime> OpeningTimes { get; set; }
         public List<DateTime> ClosingTimes { get; set; }
     }
